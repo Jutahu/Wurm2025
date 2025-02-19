5f3f569154b955121c930359501a7b7d91798197
Jutahu <jukka.huhtakallio@gmail.com>
2025-02-19 19:34:10 +0200

    Added local storagefile and small tweaks

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,17 @@
 #include <raylib.h>
 #include <deque>
 #include <raymath.h>
+//added raylib storage funtions
+#define STORAGE_DATA_FILE   "score.txt"
+
+typedef enum {
+    STORAGE_POSITION_SCORE      = 0,
+    STORAGE_POSITION_HISCORE    = 1
+} StorageData;
+
+// Persistent storage functions
+static bool SaveStorageValue(unsigned int position, int value);
+static int LoadStorageValue(unsigned int position);
 
 using namespace std;
 
@@ -119,9 +130,9 @@ class Game
 public:
     Snake snake = Snake();
     Food food = Food(snake.body);
-    bool running = true;
+    bool running = false;
     int score = 0;
-    int hiscore = 0;
+    int hiscore = LoadStorageValue(STORAGE_POSITION_HISCORE);
 
     Sound eatSound;
     Sound wallSound;
@@ -187,6 +198,7 @@ public:
         if (score>=hiscore)
         {
         hiscore = score;
+        SaveStorageValue(STORAGE_POSITION_HISCORE, hiscore);
         }
         score = 0;
         PlaySound(wallSound);
@@ -219,7 +231,10 @@ int main () {
             allowMove = true;
             game.Update();
         }
-
+        if (IsKeyPressed(KEY_P)) // PAUSE
+        {
+            game.running = false; allowMove = true;
+        }
         if (IsKeyPressed(KEY_UP) && game.snake.direction.y != 1 && allowMove)
         {
             game.snake.direction = {0, -1};
@@ -247,6 +262,12 @@ int main () {
 
         ClearBackground(WHITE);
         DrawRectangleLinesEx(Rectangle{(float)offset-5, (float)offset-5, (float)cellSize*cellCount+10, (float)cellSize*cellCount+10}, 5, LIME);
+        if (game.running==false)
+        {
+            int position = (cellSize * cellCount / 2) - offset;
+            DrawText("Press any key" , position, position, 40, darkGreen);
+            DrawText("to start" , position, position + 40, 40, PINK);
+        }
         DrawText("Upea matopeliluomus" , offset -5, 20, 40, darkGreen);
         DrawText(TextFormat("%i", game.score), offset - 5, offset + cellSize * cellCount + 10, 40, darkGreen);
         DrawText(TextFormat("HISCORE: %i", game.hiscore), offset + 530, offset + cellSize * cellCount + 10, 40, darkGreen);
@@ -257,4 +278,94 @@ int main () {
 
     CloseWindow();
     return 0;
+}
+bool SaveStorageValue(unsigned int position, int value)
+{
+    bool success = false;
+    int dataSize = 0;
+    unsigned int newDataSize = 0;
+    unsigned char *fileData = LoadFileData(STORAGE_DATA_FILE, &dataSize);
+    unsigned char *newFileData = NULL;
+
+    if (fileData != NULL)
+    {
+        if (dataSize <= (position*sizeof(int)))
+        {
+            // Increase data size up to position and store value
+            newDataSize = (position + 1)*sizeof(int);
+            newFileData = (unsigned char *)RL_REALLOC(fileData, newDataSize);
+
+            if (newFileData != NULL)
+            {
+                // RL_REALLOC succeded
+                int *dataPtr = (int *)newFileData;
+                dataPtr[position] = value;
+            }
+            else
+            {
+                // RL_REALLOC failed
+                TraceLog(LOG_WARNING, "FILEIO: [%s] Failed to realloc data (%u), position in bytes (%u) bigger than actual file size", STORAGE_DATA_FILE, dataSize, position*sizeof(int));
+
+                // We store the old size of the file
+                newFileData = fileData;
+                newDataSize = dataSize;
+            }
+        }
+        else
+        {
+            // Store the old size of the file
+            newFileData = fileData;
+            newDataSize = dataSize;
+
+            // Replace value on selected position
+            int *dataPtr = (int *)newFileData;
+            dataPtr[position] = value;
+        }
+
+        success = SaveFileData(STORAGE_DATA_FILE, newFileData, newDataSize);
+        RL_FREE(newFileData);
+
+        TraceLog(LOG_INFO, "FILEIO: [%s] Saved storage value: %i", STORAGE_DATA_FILE, value);
+    }
+    else
+    {
+        TraceLog(LOG_INFO, "FILEIO: [%s] File created successfully", STORAGE_DATA_FILE);
+
+        dataSize = (position + 1)*sizeof(int);
+        fileData = (unsigned char *)RL_MALLOC(dataSize);
+        int *dataPtr = (int *)fileData;
+        dataPtr[position] = value;
+
+        success = SaveFileData(STORAGE_DATA_FILE, fileData, dataSize);
+        UnloadFileData(fileData);
+
+        TraceLog(LOG_INFO, "FILEIO: [%s] Saved storage value: %i", STORAGE_DATA_FILE, value);
+    }
+
+    return success;
+}
+
+// Load integer value from storage file (from defined position)
+// NOTE: If requested position could not be found, value 0 is returned
+int LoadStorageValue(unsigned int position)
+{
+    int value = 0;
+    int dataSize = 0;
+    unsigned char *fileData = LoadFileData(STORAGE_DATA_FILE, &dataSize);
+
+    if (fileData != NULL)
+    {
+        if (dataSize < ((int)(position*4))) TraceLog(LOG_WARNING, "FILEIO: [%s] Failed to find storage position: %i", STORAGE_DATA_FILE, position);
+        else
+        {
+            int *dataPtr = (int *)fileData;
+            value = dataPtr[position];
+        }
+
+        UnloadFileData(fileData);
+
+        TraceLog(LOG_INFO, "FILEIO: [%s] Loaded storage value: %i", STORAGE_DATA_FILE, value);
+    }
+
+    return value;
 }
